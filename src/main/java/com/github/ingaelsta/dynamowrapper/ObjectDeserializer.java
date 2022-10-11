d22fed27c964ff467ee4ce1d69e3d6a9277ea284
IngaElsta <inga.elsta@gmail.com>
2022-10-11 12:28:29 +0300

    Restructuring methods

--- src/main/java/com/github/ingaelsta/dynamowrapper/ObjectDeserializer.java
+++ src/main/java/com/github/ingaelsta/dynamowrapper/ObjectDeserializer.java
@@ -1,5 +1,14 @@
 package com.github.ingaelsta.dynamowrapper;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
 public class ObjectDeserializer {
 
     public static class WrongTypeException extends RuntimeException {
@@ -20,33 +29,41 @@ public class ObjectDeserializer {
         }
         //possible string
         if (json.charAt(0) == '\"') {
-            if (clazz != String.class) {
-                throw new WrongTypeException (String.format("Expected %s but got String", clazz.getName()));
-            }
-            if (json.charAt(json.length() - 1) != '\"') {
-                throw new  WrongSyntaxException("String not finished properly");
-            }
-            return (T) json.substring(1, json.length() - 1);
+            return parseString(json, clazz);
         }
         //possible number
         if (Character.isDigit(json.charAt(0))) {
-            if (!(Number.class.isAssignableFrom(clazz))) {
-                throw new WrongTypeException (String.format("Expected %s but got Number", clazz.getName()));
-            }
-            if (clazz == Integer.class) {
-                return (T) Integer.valueOf(json);
-            }
-            if (clazz == Long.class) {
-                return (T) Long.valueOf(json);
-            }
-            if (clazz == Float.class) {
-                return (T) Float.valueOf(json);
-            }
-            if (clazz == Double.class) {
-                return (T) Double.valueOf(json);
-            }
-            throw new FeatureNotImplementedException("%s is a number that can't be parsed");
+            return parseNumber(json, clazz);
         }
         throw new FeatureNotImplementedException("%s maps to a class not implemented yet");
+    }
+
+    private static <T> T parseNumber(String json, Class<T> clazz) {
+        if (!(Number.class.isAssignableFrom(clazz))) {
+            throw new WrongTypeException (String.format("Expected %s but got Number", clazz.getName()));
+        }
+        if (clazz == Integer.class) {
+            return (T) Integer.valueOf(json);
+        }
+        if (clazz == Long.class) {
+            return (T) Long.valueOf(json);
+        }
+        if (clazz == Float.class) {
+            return (T) Float.valueOf(json);
+        }
+        if (clazz == Double.class) {
+            return (T) Double.valueOf(json);
+        }
+        throw new FeatureNotImplementedException("%s is a number that can't be parsed");
+    }
+
+    private static <T> T parseString(String json, Class<T> clazz) {
+        if (clazz != String.class) {
+            throw new WrongTypeException (String.format("Expected %s but got String", clazz.getName()));
+        }
+        if (json.charAt(json.length() - 1) != '\"') {
+            throw new  WrongSyntaxException(String.format("String %s not finished properly", json));
+        }
+        return (T) json.substring(1, json.length() - 1);
     }
 }
