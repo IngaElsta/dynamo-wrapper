e46679b3e33acd9cce35e797db1a8325a1c7e5f1
IngaElsta <inga.elsta@gmail.com>
2022-10-11 14:48:38 +0300

    Creating a stub for POJO deserialization

--- src/main/java/com/github/ingaelsta/dynamowrapper/ObjectDeserializer.java
+++ src/main/java/com/github/ingaelsta/dynamowrapper/ObjectDeserializer.java
@@ -38,7 +38,12 @@ public class ObjectDeserializer {
             return parseList(json, clazz);
 
         }
+        if (json.charAt(0) == '{') {
+            T result = parsePOJO(json, clazz);
+            if (result != null) return result;
+        }
         throw new FeatureNotImplementedException(String.format("Deserializing %s has not been implemented", clazz.getName()));
+
     }
 
     private static <T> T parseList(String json, Class<T> clazz) {
@@ -55,10 +60,9 @@ public class ObjectDeserializer {
 
         //or list
 
-        //or something within {}
+        //or something else within {}
 
-        //or something else - either corrupt or unsupported.
-        //todo: implement all the others so something can be returned
+        //or complete gibberish - throw error
         throw new FeatureNotImplementedException(String.format("The contents of json are either corrupt or of unsupported type: \n%s", json));
     }
 
@@ -89,5 +93,44 @@ public class ObjectDeserializer {
             throw new  WrongSyntaxException(String.format("String %s not finished properly", json));
         }
         return (T) json.substring(1, json.length() - 1);
+    }
+
+    private static <T> T parsePOJO(String json, Class<T> clazz) {
+        //get all the field names and values and add them to a list
+        List<Field> fieldList = Arrays.asList(clazz.getDeclaredFields());
+        //get all field names and their json strings
+        json = json.substring(1, json.length() - 1);
+        Map <String, Object> fieldContents = new HashMap<>();
+        while (json.length() > 0) {
+            //todo: implement parsing field names and values from json
+        }
+        //once the values are gathered, create an object and
+        Constructor [] constructors = clazz.getDeclaredConstructors();
+        if (constructors.length == 0) {
+            throw new RuntimeException(String.format("No constructors found for class: %s", clazz));
+        }
+        Constructor constructor = (Constructor <T>) constructors[0];
+        constructor.setAccessible(true);
+        T result;
+        try{
+            result = (T) constructor.newInstance(null);
+            setFields(result, fieldContents, fieldList);
+            return result;
+        } catch (Exception e) {
+            //todo: try to find another constructor that works with available field contents and then set the fields
+        }
+        return null;
+    }
+
+    private static <T> void setFields(T t, Map<String, Object> fieldContents, List<Field> fieldList) {
+        fieldList.stream()
+                .forEach(field -> {
+                    Object fieldContent = fieldContents.get(field.getName());
+                    field.setAccessible(true);try {
+                            field.set(t, fieldContent);
+                        } catch (IllegalAccessException e) {
+                            throw new RuntimeException(e);
+                        }
+                });
     }
 }
